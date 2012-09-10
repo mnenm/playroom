18f99c0bfb37f1bc3810270ed1758c26dfba886a
mnenm <naamjp@gmail.com>
2012-09-11 01:48:01 +0900

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playroom::Application.config.secret_token = '5f2dda93ff11460423ac98d670a4c47d69dc9785c6696c2f5ec0410338e930312d846245d37c1f13e072a0577d30245c49d8cb9027a86f4ebe865c87acf267f5'
